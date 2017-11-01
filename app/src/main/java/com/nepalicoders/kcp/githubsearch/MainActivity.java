6b693238359be8e60c9ce73ef0767aac2dfdeeee
Kshitij <kcpoudyal@gmail.com>
2017-11-01 03:26:12 -0500

    saving data in bundle so store data when screen is on pause

--- app/src/main/java/com/nepalicoders/kcp/githubsearch/MainActivity.java
+++ app/src/main/java/com/nepalicoders/kcp/githubsearch/MainActivity.java
@@ -1,25 +1,27 @@
 package com.nepalicoders.kcp.githubsearch;
 
-import android.net.Uri;
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import java.io.IOException;
 import java.net.URL;
-
 import utilities.NetworkUtils;
 
 public class MainActivity extends AppCompatActivity {
     EditText mSearchBoxEditText;
     TextView mUrlDisplayTextView;
     TextView mSearchResultsTextView;
+    TextView mErrorMessageTextView;
+    ProgressBar mProgressBar;
+
+    // TODO (24) Create a ProgressBar variable to store a reference to the ProgressBar
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,15 +30,54 @@ public class MainActivity extends AppCompatActivity {
         mSearchBoxEditText = (EditText) findViewById(R.id.et_search_box);
         mUrlDisplayTextView = (TextView) findViewById(R.id.tv_url_display);
         mSearchResultsTextView = (TextView) findViewById(R.id.tv_github_search_results_json);
+        mErrorMessageTextView = (TextView) findViewById(R.id.tv_error_message_display);
+        mProgressBar = (ProgressBar) findViewById(R.id.pb_loading_indicator);
+
+        // TODO (25) Get a reference to the ProgressBar using findViewById
     }
 
-    public void makeGithubSearchQuery(){
+    private void makeGithubSearchQuery() {
         URL url = NetworkUtils.buildUrl(mSearchBoxEditText.getText().toString());
         mUrlDisplayTextView.setText(url.toString());
         new GithubQueryTask().execute(url);
     }
 
-    public class GithubQueryTask extends AsyncTask<URL,Void,String>{
+    private void showJsonDataView() {
+        mSearchResultsTextView.setVisibility(View.VISIBLE);
+        mErrorMessageTextView.setVisibility(View.INVISIBLE);
+    }
+
+    private void showErrorMessage() {
+        mSearchResultsTextView.setVisibility(View.INVISIBLE);
+        mErrorMessageTextView.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int selectedMenu = item.getItemId();
+        if (selectedMenu == R.id.action_search) {
+            makeGithubSearchQuery();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public class GithubQueryTask extends AsyncTask<URL, Void, String> {
+
+        // TODO (26) Override onPreExecute to set the loading indicator to visible
+
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mProgressBar.setVisibility(View.VISIBLE);
+        }
 
         @Override
         protected String doInBackground(URL... params) {
@@ -52,26 +93,14 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(String s) {
-            if(s!=null && s!=""){
+            // TODO (27) As soon as the loading is complete, hide the loading indicator
+            mProgressBar.setVisibility(View.INVISIBLE);
+            if (s != null && s != "") {
+                showJsonDataView();
                 mSearchResultsTextView.setText(s);
+            } else {
+                showErrorMessage();
             }
         }
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main,menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int selectedMenu = item.getItemId();
-        if(selectedMenu == R.id.action_search){
-            makeGithubSearchQuery();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
